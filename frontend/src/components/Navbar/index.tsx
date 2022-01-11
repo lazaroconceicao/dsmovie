8ec74968e20ab9211d5766aee9c644f2d2f1f1ae
lazaroconceicao <joselazaroc@gmail.com>
2022-01-11 12:14:57 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
           <a href="https://github.com/lazaroconceicao">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/lazaroconceicao</p>
             </div>
           </a>
         </div>
