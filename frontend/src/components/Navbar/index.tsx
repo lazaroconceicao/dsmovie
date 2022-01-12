b265ecb80825af40e8eb0b488013ec01ade8dc84
lazaroconceicao <joselazaroc@gmail.com>
2022-01-12 11:19:13 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/lazaroconceicao">
+          <a href="https://github.com/lazaroconceicao" target="new">
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/lazaroconceicao</p>
